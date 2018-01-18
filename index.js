f630cbb734bdb3432269020eea3b4cfe44109cee
Justin Patriquin <justin@sidestory.email>
2018-01-18 17:09:05 -0400

    bugs and getJob

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var pendingJobs = Array();
 
 const web3 = new Web3(new Web3.providers.HttpProvider(ethereumUrl));
 var contractAddress = '0xd60e1a150b59a89a8e6e6ff2c03ffb6cb4096205'
+contractAddress = secret.contractAddress;
 
 // setup contract
 if(config.network != "local") {
@@ -37,7 +38,6 @@ if(config.network != "local") {
 
   setupServer()
 } else {
-
   const input = fs.readFileSync(config.solFile);
   const output = solc.compile(input.toString(), 1);
   const bytecode = output.contracts[':TrainingGrid'].bytecode;
@@ -45,20 +45,22 @@ if(config.network != "local") {
 
   var contract = new web3.eth.Contract(abi);
 
-  // TODO shouldn't deploy every time!!!!! doesn't matter locally though
-  contract.deploy({
-    data: bytecode
-  })
-  .send({
-    from: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
-    gas: 1500000,
-    gasPrice: '30'
-  }, function(error, transactionHash) { if(error) console.log(error); })
-  .on('error', function(error){ if(error) console.log(error); })
-  .then(function(newContractInstance){
-    contract = new web3.eth.Contract(abi, newContractInstance.options.address);
-    contractAddress = newContractInstance.options.address;
-
+  if(contractAddress == null) {
+    contract.deploy({
+      data: bytecode
+    })
+    .send({
+      from: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57',
+      gas: 1500000,
+      gasPrice: '30'
+    }, function(error, transactionHash) { if(error) console.log(error); })
+    .on('error', function(error){ if(error) console.log(error); })
+    .then(function(newContractInstance){
+      contract = new web3.eth.Contract(abi, newContractInstance.options.address);
+      contractAddress = newContractInstance.options.address;
+    });
+  } else {
+    contract = new web3.eth.Contract(abi, contractAddress);
     web3.eth.accounts.wallet.add(secret.privateKey);
 
     contract.methods.countExperiments().call().then(jobs => {
@@ -67,7 +69,7 @@ if(config.network != "local") {
     .catch(err => console.log(err));
 
     setupServer()
-  });
+  }
 }
 
 function success(res, obj) {
@@ -75,7 +77,7 @@ function success(res, obj) {
   res.setHeader('Content-Type', 'text/plain');
 
   if(obj != null){
-    res.end(obj);
+    res.end(JSON.stringify(obj));
   } else {
     res.end();
   }
@@ -87,28 +89,34 @@ function setupServer() {
     console.log("got request", req.url);
 
     var obj = url.parse(req.url, true)
+    console.log(obj.pathname);
     var pathname = obj.pathname
     var q = obj.query
 
-    if(obj.pathname == "addExperiment") {
+    if(obj.pathname == "/addExperiment") {
       var experimentAddress = q.experimentAddress;
       var jobAddresses = q.jobAddresses;
 
       addExperiment(experimentAddress, jobAddresses, () => {
         success(res);
       });
-    } else if(obj.pathname == "getExperiment") {
+    } else if(obj.pathname == "/getExperiment") {
       var experimentAddress = q.experimentAddress;
 
       var experiment = getExperiment(experimentAddress, (experiment) => {
         success(res, experiment)
       });
-    } else if(obj.pathname == "getAvailableJob") {
-      var job = getAvailableJob((job) => {
-        console.log("GET JOB", job);
+    } else if(obj.pathname == "/getAvailableJobId") {
+      var job = getAvailableJobId((job) => {
+        console.log("GET JOB ID", job);
         success(res, job);
       });
-    } else if(obj.pathname == "addResult") {
+    } else if(obj.pathname == "/getJob") {
+        var job = getJob((job) => {
+          console.log("GET JOB", job);
+          success(res, job);
+        });
+    } else if(obj.pathname == "/addResult") {
       var experimentAddress = q.experimentAddress;
       var jobAddress = q.jobAddress;
       var resultAddress = q.resultAddress;
@@ -116,7 +124,7 @@ function setupServer() {
       addResult(jobAddress, resultAddress, () => {
         success(res);
       });
-    } else if (obj.pathname == "getResults") {
+    } else if (obj.pathname == "/getResults") {
         var jobAddress = q.jobAddress;
 
         getResults(jobAddress, (result) => {
@@ -134,7 +142,6 @@ function setupServer() {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
 }
-
 
 function addressToArray (ipfsAddress) {
   const targetLength = 64 // fill the address with 0 at the end to this length
@@ -164,7 +171,6 @@ function sendTransaction(data) {
   } else {
     f = specificNetworkAddress;
   }
-
 
   const params = {
     from: f,
@@ -206,30 +212,48 @@ function addExperiment(experimentAddress, jobAddresses, cb) {
 function getExperiment(experimentAddress, cb) {
   var experimentAddress = addressToArray(config.experimentAddress);
 
-  contract.methods.getExperiment().call({from:specificNetworkAddress}).then(experiment => {
+  contract.methods.getExperiment().call().then(experiment => {
     // TODO make this into some sort of JSON object???
     cb(experiment);
   })
 }
 
-function getAvailableJob() {
-  contract.methods.getAvailableJobIds().call({from: specificNetworkAddress}).then(res => {
-    var allJobs = res[0];
-    // stupid filtering!
+function getAvailableJobId(cb) {
+  var json = {
+    jobId: ""
+  };
 
-    var zero = web3.utils.padRight(web3.utils.fromAscii("\0"), 66, "0");
-    someJobs = _.filter(allJobs[0], (job) => {
-      job !== zero
-    });
+  contract.methods.getAvailableJobIds().call().then(res => {
+    if(res.length > 0) {
+      var allJobs = res[0];
 
-    job = _.sample(someJobs);
+      // stupid filtering!
+      var zero = web3.utils.padRight(web3.utils.fromAscii("\0"), 66, "0");
+      someJobs = _.filter(allJobs[0], (job) => {
+        job !== zero
+      });
 
-    var json = {
-      jobAddress: arrayToAddress(job)
-    };
+      job = _.sample(someJobs);
+
+      json.jobAddress = job;
+    }
 
     cb(json);
   })
+}
+
+function getJob(jobId, cb) {
+  var json = {
+    jobAddress: ""
+  }
+
+  contract.methods.getJob(jobId).call().then(res => {
+    if(res.length > 0) {
+      json.jobAddress = arrayToAddress(res[2]);
+    }
+
+    cb(json);
+  });
 }
 
 function addResult(jobAddress, resultAddress, cb) {
@@ -250,12 +274,20 @@ function getResults(jobAddress, cb) {
 
   var jobId = web3utils.soliditySha3(jobData);
 
-  contract.methods.getResults().call({from: specificNetworkAddress}).then(res => {
-    var json = {
-      owner: res[0],
-      resultAddress: res[1]
+  var json = {
+    owner: "",
+    resultAddress: ""
+  }
+
+  contract.methods.getResults().call().then(res => {
+    if(res.length > 0) {
+      var json = {
+        owner: res[0],
+        resultAddress: res[1]
+      }
     }
-    cb(result);
+
+    cb(json);
   });
 }
 
